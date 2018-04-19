764eb83a462345ccc70ae45ec49a49dcd02c480f
Honam <walkingtospace@gmail.com>
2018-04-19 01:19:30 -0700

    Format organization as a ramp-up tas

--- app/index.js
+++ app/index.js
@@ -1,3 +1,15 @@
+/*
+ * REST API public entry point on block chain.
+ * API descriptions 0
+ * /balance: return current balance persisted in the current node wallet
+ * /blocks: return the current block chain persisted in this node
+ * /mine: add a newly-received block to the current chain
+ * /transactions: return current transaction pool contents
+ * /transact: create a new transaction and broadcast to all connected nodes
+ * /mine-transactions: mine the block and add to the block chain
+ * /public-key: return wallet's public key
+ */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
@@ -8,7 +20,6 @@ const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
-
 const app = express();
 
 const bc = new Blockchain(); 
@@ -18,47 +29,46 @@ const p2pServer = new P2pServer(bc, tp);
 const miner = new Miner(bc,tp,wallet, p2pServer);
 
 app.use(bodyParser.json());
-app.get('/balance', (req,res)=>{
-     res.json([{balance:JSON.stringify(wallet.calculateBalance(bc))}])
 
+app.get('/balance', (req,res) => {
+     res.json([{balance:JSON.stringify(wallet.calculateBalance(bc))}])
 });
+
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
-
 });
 
-app.post('/mine',(req, res) => {
+app.post('/mine' , (req, res) => {
     const block = bc.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
-
+    console.log(`New block is added: ${block.toString()}`);
     p2pServer.syncChain();
 
     res.redirect('/blocks');
 });
 
-app.get('/transactions', (req,res)=>{
+app.get('/transactions', (req,res) => {
      res.json(tp.transactions);
 });
 
-app.post('/transact', (req,res)=>{
+app.post('/transact', (req,res) => { //calculate received transaction
   const { recipient, amount } = req.body;
   const transaction = wallet.createTransaction(recipient, amount, bc, tp);
   p2pServer.broadcastTransaction(transaction);
 
   res.redirect('/transactions');
-
 });
-   app.get('/mine-transactions', (req,res)=>{
+
+app.get('/mine-transactions', (req,res) => {
      const block = miner.mine();
      console.log(`New block added: ${block.toString()}`);
+
      res.redirect('./blocks');
 });
 
-
-   app.get('/public-key',(req,res)=>{
+app.get('/public-key',(req,res) => {
    res.json([{address:wallet.publicKey}]);
 });
 
-app.listen(HTTP_PORT, ()=>console.log(`Listening on port ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
 
--- app/miner.js
+++ app/miner.js
@@ -2,27 +2,31 @@ const Wallet = require('../wallet');
 const Transaction = require('../wallet/transaction');
 
 class Miner {
-  constructor(blockchain, transactionPool, wallet, p2pServer){
-   this.blockchain = blockchain;
-   this.transactionPool =transactionPool;
-   this.wallet=wallet;
-   this.p2pServer = p2pServer;
+  constructor(blockchain, transactionPool, wallet, p2pServer) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.p2pServer = p2pServer;
+  }
 
-}
-  mine(){
-   const validTransactions = this.transactionPool.validTransactions();
-   validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()));
+  /*
+   * Mining behavior definition:
+     1. get transactions from the pool 
+     2. add a new block to the existing block chain
+     3. sync-up chains
+     4. broadcast a mining result via p2pServer 
+   */
+  mine() {
+    const validTransactions = this.transactionPool.validTransactions();
+    validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()));
    
+    const block = this.blockchain.addBlock(validTransactions);
+    this.p2pServer.syncChain();
+    this.transactionPool.clear();
+    this.p2pServer.broadcastClearTransactions();
 
-  const block = this.blockchain.addBlock(validTransactions);
-  this.p2pServer.syncChain();
-  this.transactionPool.clear();
-  this.p2pServer.broadcastClearTransactions();
-
-  return block;
-}
-
-
+    return block;
+  }
 }
 
 module.exports = Miner;
